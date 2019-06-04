8c971e63d0ddf232678d5a0bf19f9fd12858e259
Doru-Florin Blanzeanu <blanzeanu.doru@gmail.com>
2019-06-04 13:51:17 +0300

    Changes ld alias

--- gitconfig.sh
+++ gitconfig.sh
@@ -9,7 +9,7 @@ git config --global alias.st "status"
 
 # log decorators
 git config --global alias.l "log --decorate --graph --oneline"
-git config --global alias.ld "log --decorate --graph --oneline --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.ld "log --decorate --graph --oneline --pretty=format:'%C(bold red)%h%Creset %Cgreen(%cr) %C(bold blue)<%aN>%Creset %s %C(yellow)%d%Creset' --abbrev-commit"
 git config --global alias.lg "log --graph --pretty=format:'commit: %C(bold red)%h%Creset %C(red)<%H>%Creset %C(bold magenta)%d %Creset%ndate: %C(bold yellow)%cd %Creset%C(yellow)%cr%Creset%nauthor: %C(bold blue)%an%Creset %C(blue)<%ae>%Creset%n%C(cyan)%s%n%Creset'"
 git config --global alias.me "!git lg --author='Doru Blanzeanu'"
 
